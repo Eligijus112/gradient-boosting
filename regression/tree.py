2d8f6ab35eb6a33851ee5fc293e0aa6e743a7479
eligijus <elasorama@gmail.com>
2022-03-24 19:11:25 +0200

    Added the founding of the spliting criteria

--- regression/tree.py
+++ regression/tree.py
@@ -1,11 +1,19 @@
+# Data wrangling
+import pandas as pd 
+
+# OS traversal 
+import os 
+
+
 class Tree():
     """
     Class to fit a regression tree to the given data 
     """
     def __init__(
         self, 
-        Y: list,
-        X: dict,
+        d: pd.DataFrame,
+        y_var: str,
+        x_vars: list,
         max_depth: int = 4
     ):
         """
@@ -13,28 +21,27 @@ class Tree():
 
         Arguments
         ---------
-        Y: list
+        d: pd.DataFrame
+            The dataframe to create the tree from
+        y_var: str
             The target values
-        X: dict
-            The features in the form of: 
-            {
-                ...
-                ,
-                'feature_name': {
-                    'values': list
-                    'type': object or numeric
-                },
-                ...
-            }
+        x_vars: dict
+            The features to use in the tree
         max_depth: int
             The maximum depth of the tree
         """
+        # Saving the names of y variable and X features
+        self.y_var = y_var 
+        self.features = x_vars
+
+        # Saving the node data to memory 
+        self.d = d[[y_var] + x_vars] 
+
         # Saving the data to the node 
-        self.Y = Y 
-        self.X = X
+        self.Y = d[y_var].values.tolist()
 
         # Saving the number of observations in the node 
-        self.n = len(Y)
+        self.n = len(d)
 
         # Initiating the depth counter
         self.depth = 0
@@ -42,193 +49,183 @@ class Tree():
         # Saving the maximum depth of the tree
         self.max_depth = max_depth
 
-        # Getting the mean 
+        # Calculating the mse of the node 
+        self.get_y_mse()
+
+        # Infering the best split 
+        self.get_best_split()
+
+        # Saving to memory the y mean (prediction of the node)
         self.get_y_mean()
 
-        # Calculating the current residuals
-        self.residuals = self.Y - self.y_mean
-
-        # Calculating the mse of the node 
-        self.mse = self.get_mse(Y, self.ymean)
-
-        # Initiating the left and right nodes as empty nodes
-        self.left = None 
-        self.right = None 
-
-        # Default values for splits
-        self.best_feature = None 
-        self.best_value = None 
-
-    def get_y_mean(self) -> float:
+    @staticmethod
+    def get_mean(x: list) -> float:
         """
-        Method that calculates the mean of the Y values in the current tree
+        Calculates the mean over a list of float elements
         """
         # Initiating the sum counter 
-        y_sum = 0 
+        _sum = 0 
+
+        # Infering the lenght of list 
+        _n = len(x)
 
         # Iterating through the y values
-        for y in self.Y:
-            y_sum += y
+        for _x in x:
+            _sum += _x
+
+        # Returning the mean 
+        return _sum / _n 
+
+    def get_y_mean(self) -> None:
+        """
+        Saves the current node's mean
+        """
+        self.y_mean = self.get_mean(self.Y)
+
+    def get_mse(self, x: list) -> float: 
+        """
+        Calculates the mse of a given list by subtracting the mean, 
+        summing and dividing by n
+        """
+        # Infering the lenght of list 
+        _n = len(x)
 
         # Calculating the mean 
-        y_mean = y_sum / self.n
+        _mean = self.get_mean(x)
 
-        # Saving the mean to memory 
-        self.y_mean = y_mean
+        # Getting the residuals 
+        residuals = [_x - _mean for _x in x]
 
-    def get_mse(self) -> float:
-        """
-        Method to calculate the mean squared error 
-        """
-        # Squering the residuals 
-        residuals = self.residuals ** 2
+        # Squaring the residuals
+        residuals = [r ** 2 for r in residuals]
 
-        # Summing all the residuals
-        r_sum = 0 
+        # Summing the residuals 
+        _r_sum = 0 
         for r in residuals:
-            r_sum += r        
+            _r_sum += r
 
-        # Getting the average and returning 
-        return r_sum / self.n
+        # Returning the mean squared error 
+        return _r_sum / _n      
 
-    @staticmethod
-    def ma(x: np.array, window: int) -> np.array:
+    def get_y_mse(self) -> None:
         """
-        Calculates the moving average of the given list. 
+        Method to calculate the MSE of the current node
+        """ 
+        self.mse = self.get_mse(self.Y)
+
+    def get_mse_weighted(self, y_left: list, y_right: list):
         """
-        return np.convolve(x, np.ones(window), 'valid') / window
-
-    def best_split(self) -> tuple:
+        Calculates the weighted mse given two lists
         """
-        Given the X features and Y targets calculates the best split 
-        for a decision tree
+        # Calculating the lenth of both values
+        _n_left = len(y_left)
+        _n_right = len(y_right)
+        _n_total = _n_left + _n_right
+
+        # Calculating the mse of each sides
+        _mse_left = self.get_mse(y_left)
+        _mse_right = self.get_mse(y_right)
+
+        # Calculating the weighted mse 
+        return (_mse_left * _n_left / _n_total) + (_mse_right * _n_right / _n_total) 
+
+    def get_best_split(self):
         """
-        # Creating a dataset for spliting
-        df = self.X.copy()
-        df['Y'] = self.Y
-
-        # Getting the GINI impurity for the base input 
-        mse_base = self.mse
-
-        # Finding which split yields the best GINI gain 
-        #max_gain = 0
-
-        # Default best feature and split
-        best_feature = None
-        best_value = None
-
-        for feature in self.features:
-            # Droping missing values
-            Xdf = df.dropna().sort_values(feature)
-
-            # Sorting the values and getting the rolling average
-            xmeans = self.ma(Xdf[feature].unique(), 2)
-
-            for value in xmeans:
-                # Getting the left and right ys 
-                left_y = Xdf[Xdf[feature]<value]['Y'].values
-                right_y = Xdf[Xdf[feature]>=value]['Y'].values
-
-                # Getting the means 
-                left_mean = np.mean(left_y)
-                right_mean = np.mean(right_y)
-
-                # Getting the left and right residuals 
-                res_left = left_y - left_mean 
-                res_right = right_y - right_mean
-
-                # Concatenating the residuals 
-                r = np.concatenate((res_left, res_right), axis=None)
-
-                # Calculating the mse 
-                n = len(r)
-                r = r ** 2
-                r = np.sum(r)
-                mse_split = r / n
-
-                # Checking if this is the best split so far 
-                if mse_split < mse_base:
-                    best_feature = feature
-                    best_value = value 
-
-                    # Setting the best gain to the current one 
-                    mse_base = mse_split
-
-        return (best_feature, best_value)
-
-    def grow_tree(self):
-        """
-        Recursive method to create the decision tree
-        """
-        # Making a df from the data 
-        df = self.X.copy()
-        df['Y'] = self.Y
-
-        # If there is GINI to be gained, we split further 
-        if (self.depth < self.max_depth) and (self.n >= self.min_samples_split):
-
-            # Getting the best split 
-            best_feature, best_value = self.best_split()
-
-            if best_feature is not None:
-                # Saving the best split to the current node 
-                self.best_feature = best_feature
-                self.best_value = best_value
-
-                # Getting the left and right nodes
-                left_df, right_df = df[df[best_feature]<=best_value].copy(), df[df[best_feature]>best_value].copy()
-
-                # Creating the left and right nodes
-                left = NodeRegression(
-                    left_df['Y'].values.tolist(), 
-                    left_df[self.features], 
-                    depth=self.depth + 1, 
-                    max_depth=self.max_depth, 
-                    min_samples_split=self.min_samples_split, 
-                    node_type='left_node',
-                    rule=f"{best_feature} <= {round(best_value, 3)}"
-                    )
-
-                self.left = left 
-                self.left.grow_tree()
-
-                right = NodeRegression(
-                    right_df['Y'].values.tolist(), 
-                    right_df[self.features], 
-                    depth=self.depth + 1, 
-                    max_depth=self.max_depth, 
-                    min_samples_split=self.min_samples_split,
-                    node_type='right_node',
-                    rule=f"{best_feature} > {round(best_value, 3)}"
-                    )
-
-                self.right = right
-                self.right.grow_tree()
-
-    def print_info(self, width=4):
-        """
-        Method to print the infromation about the tree
-        """
-        # Defining the number of spaces 
-        const = int(self.depth * width ** 1.5)
-        spaces = "-" * const
+        Method to find the best split among the features 
         
-        if self.node_type == 'root':
-            print("Root")
-        else:
-            print(f"|{spaces} Split rule: {self.rule}")
-        print(f"{' ' * const}   | MSE of the node: {round(self.mse, 2)}")
-        print(f"{' ' * const}   | Count of observations in node: {self.n}")
-        print(f"{' ' * const}   | Prediction of node: {round(self.ymean, 3)}")   
+        The logic is to find the feature and the feature value which reduces 
+        the objects mse the most 
+        """
+        # Setting initial values
+        _best_mse = self.mse
+        _best_feature = None
+        _best_feature_value = None
 
-    def print_tree(self):
-        """
-        Prints the whole tree from the current node to the bottom
-        """
-        self.print_info() 
-        
-        if self.left is not None: 
-            self.left.print_tree()
-        
-        if self.right is not None:
-            self.right.print_tree()
+        # Creating lists of categorical and numeric features
+        _cat_features = [ft for ft in self.d.columns if self.d[ft].dtype == 'category']
+        _num_features = list(set(self.features) - set(_cat_features))
+
+        # Going through the categorical features
+        for _cat_feature in _cat_features:
+            # Infering the levels of the categorical feature 
+            _levels = self.d[_cat_feature].unique()
+            
+            for _level in _levels:
+                # Spliting the data into two parts: one that is equal to the categorical level
+                # and one that is not 
+                _y_left = self.d.loc[self.d[_cat_feature]==_level, self.y_var].values
+                _y_right = self.d.loc[self.d[_cat_feature]!=_level, self.y_var].values
+
+                # Calculating the weighted mse 
+                _mse_w = self.get_mse_weighted(_y_left, _y_right)
+
+                # Checking the clause 
+                if _mse_w < _best_mse:
+                    _best_mse = _mse_w 
+                    _best_feature = _cat_feature
+                    _best_feature_value = str(_level) # Specificaly adding the type for later spliting
+
+        # Going through the numerical features
+        for _num_feature in _num_features:
+            # Getting the values 
+            _values = self.d[_num_feature].values
+
+            # Getting the unique entries
+            _values = list(set(_values))
+
+            # Sorting the values
+            _values.sort()
+
+            # Getting the rolling average values of the feature 
+            # and spliting the dataset by that value
+            for i in range(len(_values) - 1): 
+                # Roling average
+                _left = _values[i]
+                _right = _values[i + 1]
+                _mean = (_left + _right) / 2
+
+                # Iterating over the values and calculating the mse 
+                _y_left = self.d.loc[self.d[_num_feature]<=_mean, self.y_var].values
+                _y_right = self.d.loc[self.d[_num_feature]>_mean, self.y_var].values
+
+                # Getting the weighted mse 
+                _mse_w = self.get_mse_weighted(_y_left, _y_right)
+
+                # Checking the clause 
+                if _mse_w < _best_mse:
+                    _best_mse = _mse_w 
+                    _best_feature = _num_feature
+                    _best_feature_value = _mean
+
+        # Saving the best splits to object memory 
+        self.best_feature = _best_feature
+        self.best_feature_value = _best_feature_value
+
+if __name__ == '__main__':
+    # Infering the file location 
+    _current_dir = os.path.dirname(os.path.abspath(__file__))
+
+    # Defining the data directory 
+    _data_dir = os.path.join(_current_dir, 'data')
+
+    # Reading the data
+    d = pd.read_csv(os.path.join(_data_dir, 'auto-mpg.csv'))
+
+    # Encoding the correct types for categorical variables
+    d['origin'] = d['origin'].astype('category')
+
+    # Defining the y_var 
+    _y_var = 'mpg'
+
+    # Defining the features
+    _x_vars = ['origin', 'weight', 'cylinders']
+
+    # Initiating the ungrown tree 
+    tree = Tree(
+        d,
+        _y_var,
+        _x_vars,
+    )
+
+    # Root node's best split 
+    print(f"Root feature to split:\n{tree.best_feature}\nValue:\n{tree.best_feature_value}")
